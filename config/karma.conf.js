00f329e70fe5455cbc68c414d6e0e332dc273d97
harsh429 <hbhavsar993@gmail.com>
2017-04-04 07:45:34 -0700

    p_2

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -10,11 +10,7 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (e.g. files, exclude)
     basePath: '',
 
-    /*
-     * Frameworks to use
-     *
-     * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-     */
+   
     frameworks: ['jasmine'],
 
     // list of files to exclude
